1ebc3b0f8493cb0f0e9ceb7d3e4fa8d606e534bd
DanaK5 <202300513@student.polytechnic.bh>
2025-12-15 15:25:01 +0300

    header changes

---
+++ DonAte/DonAte/Controllers/Donor/sharedHeader.swift
@@ -0,0 +1,20 @@
+//
+//  sharedHeader.swift
+//  DonAte
+//
+//  Created by BP-36-201-04 on 15/12/2025.
+//
+
+import UIKit
+
+class sharedHeader: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
