a7a0e598156734da383bb447c5520dfa96030d3d
fatima <fatimaalmajid05@gmail.com>
2025-12-29 21:03:23 +0300

    some sign up screens

---
+++ DonAte/DonAte/Controllers/Donor/RegistrationViewController.swift
@@ -0,0 +1,30 @@
+//
+//  RegistrationViewController.swift
+//  DonAte
+//
+//  Created by Guest 1 on 29/12/2025.
+//
+
+import UIKit
+import FirebaseAuth
+
+class RegistrationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
