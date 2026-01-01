f84a4377ee5bf6769045ff715a74ecf9287235b9
Habib-Hub3 <202200123@student.polytechnic.bh>
2026-01-01 13:53:44 +0300

    added cloudinary to main

---
+++ DonAte/DonAte/CloudinaryManager.swift
@@ -0,0 +1,11 @@
+//
+//  CloudinaryManager.swift
+//  DonAte
+//
+//  Created by BP-36-213-18 on 01/01/2026.
+//
+
+import Foundation
+import Cloudinary
+
+let cloudinary = CLDCloudinary(configuration: CLDConfiguration(cloudName: "dldepnjvn",secure: true))
